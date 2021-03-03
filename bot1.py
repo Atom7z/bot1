16c5aad76b3f4577399d10ba36dc95aaa8703459
Atomz <Atomz.peace@gmail.com>
2021-03-03 21:31:26 +0300

    newnew

--- bot1.py
+++ bot1.py
@@ -2,12 +2,14 @@ import telebot
 
 bot = telebot.TeleBot("1480554036:AAFJpY2FclNG_o7fT5b90UK71ydsfNbwifk", parse_mode=None)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(func=lambda m: True)
+@bot.message_handler(commands=['send_nudes'])
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	chatId = message.chat.id
+	photo = open("nudes.jpg", 'rb')
+	bot.send_photo(chatId, photo)
 
 bot.polling()
